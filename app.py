9d4ccb145fed92b75822d2d25150e5e0f99e1e70
prog <evprog@gmail.com>
2023-02-12 12:18:12 +0500

    локальная страница и ajax через jQuery (нет)

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+# from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, static_url_path='/static')
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/',methods=["GET"])
 @app.route('/index',methods=["GET"])
 def index():
+    
     return "привет с сервера"
+
+
+@app.route('/url',methods=["GET"])
+def url():
+    return jsonify({'url':"temp/1.pdf"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=5000)
