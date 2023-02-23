90fd85ac123b8061efe38ecb7cadee7fb019eb57
prog <evprog@gmail.com>
2023-02-23 19:30:20 +0500

    первые 10 записей вышли

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
-# from flask_cors import CORS, cross_origin
+
 from pymongo import MongoClient
-from bson import json_util
+
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["loc-search"]
@@ -13,34 +13,10 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/',methods=["GET"])
 @app.route('/index',methods=["GET"])
-def index():
-    return "hi" ;
-
-@app.route('/get',methods=['GET'])
-def get_data():
-    data = {'name': 'John', 'age': 30}
-    return jsonify(data)
-
 @app.route('/search',methods=['GET'])
-def search():
-    # query = request.args.get('query')
-    
-    page_size = 10
-    page_number = request.args.get('page_number')
-    
-    try:
-        page_number = int(page_number)
-    except:
-        page_number = 1
-        
-    documents_to_skip = page_size * (page_number - 1)
-    
-    documents = collection.find().skip(documents_to_skip).limit(page_size)
-    
-    return jsonify(json_util.dumps(documents))
-    
- 
-    
+def index():
+    documents = collection.find().limit(10)
+    return render_template('index.html', documents=documents)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=5000)
